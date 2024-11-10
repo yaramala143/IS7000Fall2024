371a1d165d0b8edc32272c69cb4ad7005505b9fe
prachetan12 <MYalla01@indianatech.net>
2024-11-09 22:43:13 -0600

    Dashboard Placeholder

--- src/pages/dashboard/Home.js
+++ src/pages/dashboard/Home.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 function Home() {
   return (
-    <div className='text-white flex justify-center items-center w-full h-screen'>
-        <h1 className='text-4xl'>Home</h1>
+    <div className="flex flex-col items-center justify-center w-full h-screen bg-gray-100">
+      <div className="p-6 border-4 border-dashed border-gray-300 rounded-lg text-center">
+        <h1 className="text-3xl font-semibold text-gray-700">Dashboard Placeholder</h1>
+        <p className="text-gray-500 mt-2">
+          This is a placeholder for the dashboard.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
